e65923c1ea0a8827c98a080adf6a077e3a6b205d
noman2604 <nomanpatel07059@gmail.com>
2024-12-03 16:06:17 +0530

    Updated the password generator project

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import PasswordGenerator from "@/components/passwordGen";
+
+import PasswordGenerator from '@/components/passwordGen'
+
 
 export default function Home() {
   return (
-    <>
-      <main className="flex min-h-screen bg-slate-200 flex-col items-center justify-center md:p-24">
-        <PasswordGenerator />
-      </main>
-    </>
-  );
+    <main className="flex min-h-screen bg-slate-200 flex-col items-center justify-center ">
+      <PasswordGenerator/>
+    </main>
+  )
 }
